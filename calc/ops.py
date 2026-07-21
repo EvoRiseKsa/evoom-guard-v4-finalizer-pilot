03868cf84825b7aaa9bdb0efd88f215eb878b943
Mana Alharbi <e7sas_77@hotmail.com>
2026-07-22 00:25:38 +0300

    feat: add calculator subtraction primitive

--- calc/ops.py
+++ calc/ops.py
@@ -1,3 +1,8 @@
 def add(left: int, right: int) -> int:
     """Return the mathematical sum of two integers."""
     return left + right
+
+
+def subtract(left: int, right: int) -> int:
+    """Return the mathematical difference of two integers."""
+    return left - right
